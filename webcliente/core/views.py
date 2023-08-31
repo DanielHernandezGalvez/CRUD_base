d66dc8b9ce811c1a3f87ac592cd2fb93ecc8cbec
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2023-08-31 12:35:09 -0600

    vistas y urls

--- webcliente/core/views.py
+++ webcliente/core/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render, HttpResponse
 
+html_base = """
+    <h1>Mi web Personal</h1>
+    <ul>
+        <li><a href="/">Portada</a></li>
+        <li><a href="/about-me/">Acerca de</a></li>
+    </ul>
+"""
+
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>Hello world</h1><h2>Portada</h2>")
+    # html_response = "<h1>Hello world</h1>"
+    # for i in range(10):
+    #     html_response += "<h2>Portada</h2>"
+    # return HttpResponse(html_response)
+    return HttpResponse(html_base + """
+        <h2>Portada</h2>
+        <p>esto es la portada</p>
+     """)
+
+def about(request):
+    return HttpResponse(html_base + """
+        <h2>Acerca de</h2>
+        <p>Me llamo Sonia y soy Administradora</p>
+    """)
--- webcliente/webcliente/urls.py
+++ webcliente/webcliente/urls.py
@@ -19,6 +19,7 @@ from django.urls import path
 from core import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.home, name="home"),
+    path("about-me/", views.about, name="about"),
+    path('admin/', admin.site.urls),
 ]
