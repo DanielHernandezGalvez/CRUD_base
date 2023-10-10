e4976fcd85683259fee6cb79b8f1994389d326f7
unknown <hernandezgalvezalejandro@gmail.com>
2023-10-10 12:27:39 -0600

    navbar

--- components/navbar/main.js
+++
@@ -1,7 +0,0 @@
-// NAVBAR
-const navbarToggle = document.getElementById("navbarToggle");
-const navbarMenu = document.getElementById("navbarMenu");
-
-navbarToggle.addEventListener("click", () => {
-    navbarMenu.classList.toggle("active");
-});
