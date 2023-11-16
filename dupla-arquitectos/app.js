2a4ae4479460ea25afc5897ae1aadcfdd0f70a37
unknown <hernandezgalvezalejandro@gmail.com>
2023-11-16 10:57:04 -0600

    footer y redes

---
+++ dupla-arquitectos/app.js
@@ -0,0 +1,8 @@
+function getYear() {
+  const fechaActual = new Date();
+  return fechaActual.getFullYear() + " ";
+}
+
+const yearSpan = document.getElementById("year");
+
+yearSpan.textContent = getYear();
