b12bc8c5d924dcc84d3f59498907d507b183cf37
unknown <hernandezgalvezalejandro@gmail.com>
2024-01-05 18:03:45 -0600

    necesitas activar los console.log

--- aceituna/src/components/Calendar/Select.jsx
+++ aceituna/src/components/Calendar/Select.jsx
@@ -1,17 +1,22 @@
 import React, { useState } from "react";
 
-
 const Select = () => {
-    const [day, setDay] = useState(1)
+    const [day, setDay] = useState(1);
 
-    const handleDay = (event) => {
-        const clickedDay = event.target.value;
-    console.log(clickedDay);
-    setDay(clickedDay);
-    }
+    const handleDay = (clickedDay) => {
+        console.log(clickedDay);
+        setDay(clickedDay);
+    };
+
+    const handleButtonClick = () => {
+        console.log("Button clicked");
+    };
 
     return (
         <>
+
+<button onClick={handleButtonClick}>Click me</button>
+
             <h1 className="text-gray-500">Este es el Select</h1>
             <div className="w-[50vw]">
                 <table className="w-full">
@@ -27,51 +32,24 @@ const Select = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td className="border-2 border-black text-center"><button onClick={handleDay} value={1}>1</button></td>
-                            <td className="border-2 border-black text-center">2</td>
-                            <td className="border-2 border-black text-center">3</td>
-                            <td className="border-2 border-black text-center">4</td>
-                            <td className="border-2 border-black text-center">5</td>
-                            <td className="border-2 border-black text-center">6</td>
-                            <td className="border-2 border-black text-center">7</td>
-                        </tr>
-                        <tr>
-                            <td className="border-2 border-black text-center">8</td>
-                            <td className="border-2 border-black text-center">9</td>
-                            <td className="border-2 border-black text-center">10</td>
-                            <td className="border-2 border-black text-center">11</td>
-                            <td className="border-2 border-black text-center">12</td>
-                            <td className="border-2 border-black text-center">13</td>
-                            <td className="border-2 border-black text-center">14</td>
-                        </tr>
-                        <tr>
-                            <td className="border-2 border-black text-center">15</td>
-                            <td className="border-2 border-black text-center">16</td>
-                            <td className="border-2 border-black text-center">17</td>
-                            <td className="border-2 border-black text-center">18</td>
-                            <td className="border-2 border-black text-center">19</td>
-                            <td className="border-2 border-black text-center">20</td>
-                            <td className="border-2 border-black text-center">21</td>
-                        </tr>
-                        <tr>
-                            <td className="border-2 border-black text-center">22</td>
-                            <td className="border-2 border-black text-center">23</td>
-                            <td className="border-2 border-black text-center">24</td>
-                            <td className="border-2 border-black text-center">25</td>
-                            <td className="border-2 border-black text-center">26</td>
-                            <td className="border-2 border-black text-center">27</td>
-                            <td className="border-2 border-black text-center">28</td>
-                        </tr>
-                        <tr>
-                            <td className="border-2 border-black text-center">29</td>
-                            <td className="border-2 border-black text-center">30</td>
-                            <td className="border-2 border-black text-center">31</td>
-                        </tr>
+                        {[1, 2, 3, 4, 5].map((row) => (
+                            <tr key={row}>
+                                {[1, 2, 3, 4, 5, 6, 7].map((col) => (
+                                    <td
+                                        key={col}
+                                        className="border-2 border-black text-center"
+                                        onClick={() => handleDay(col + (row - 1) * 7)}
+                                    >
+                                        {col + (row - 1) * 7}
+                                    </td>
+                                ))}
+                            </tr>
+                        ))}
                     </tbody>
                 </table>
             </div>
         </>
-    )
-}
+    );
+};
+
 export default Select;
