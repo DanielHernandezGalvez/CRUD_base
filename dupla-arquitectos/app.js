e3ab9bc53b5d4d187bde4f716a11233affb38b67
unknown <hernandezgalvezalejandro@gmail.com>
2024-05-10 15:33:56 -0600

    start project

--- dupla-arquitectos/app.js
+++
@@ -1,7 +0,0 @@
-function getYear() {
-  const fechaActual = new Date();
-  return fechaActual.getFullYear() + " ";
-}
-
-const yearSpan = document.getElementById("year");
-yearSpan.textContent = getYear();
