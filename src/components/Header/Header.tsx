b3a6cd65a46693eb3aa9ba32f988d54c624e3e5b
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2024-05-24 11:59:53 -0600

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -14,6 +14,7 @@ const Header = () => {
             inmueble ideal</h1>
           <p className='text-pretty text-sm text-gray-800 my-6'>Encuentra casas,
             departamentos, ranchos, naves industriales y terrenos</p>
+          
           {/* botones */}
           <div >
             <a className='mr-6 px-6 py-2 bg-gray-800 border-2 border-gray-800  
@@ -21,6 +22,7 @@ const Header = () => {
             <a className='mr-6 px-6 py-2 bg-transparent border-2 border-gray-800  
               rounded-xl cursor-pointer text-gray-800 transition hover:bg-gray-800 hover:text-white'>Ver todo</a>
           </div>
+          
           {/* iconos */}
           <div className='mt-6'>
             <FontAwesomeIcon icon={faFacebook} className='bg-gray-800 text-xl rounded-full mr-3 p-1 text-white transition hover:scale-110' />
