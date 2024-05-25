ad735a9cfe69aa02506a6bc3e0874bf6b0d5eabe
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2024-05-25 11:25:22 -0600

    Update Carousel.tsx

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -16,12 +16,12 @@ const Carousel: React.FC = () => {
         <div className="carousel-item">
           <img src="http://fakeimg.pl/2000x800/F90/fff/?text=Carousel" alt="Carousel" />
         </div>
-        <label htmlFor="carousel-3" className="carousel-control prev control-1">‹</label>
-        <label htmlFor="carousel-2" className="carousel-control next control-1">›</label>
-        <label htmlFor="carousel-1" className="carousel-control prev control-2">‹</label>
-        <label htmlFor="carousel-3" className="carousel-control next control-2">›</label>
-        <label htmlFor="carousel-2" className="carousel-control prev control-3">‹</label>
-        <label htmlFor="carousel-1" className="carousel-control next control-3">›</label>
+          <label htmlFor="carousel-3" className="carousel-control prev control-1">‹</label>
+          <label htmlFor="carousel-2" className="carousel-control next control-1">›</label>
+          <label htmlFor="carousel-1" className="carousel-control prev control-2">‹</label>
+          <label htmlFor="carousel-3" className="carousel-control next control-2">›</label>
+          <label htmlFor="carousel-2" className="carousel-control prev control-3">‹</label>
+          <label htmlFor="carousel-1" className="carousel-control next control-3">›</label>
         <ol className="carousel-indicators">
           <li>
             <label htmlFor="carousel-1" className="carousel-bullet">•</label>
